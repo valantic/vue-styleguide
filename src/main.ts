57a226e37ff1551fd674564fc7e011eca4c6c6a0
Philip Nachbauer <hello@haba.at>
2025-03-18 15:36:55 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const vuePlugins = plugins;
 let vueOptions = options;
 
 if (import.meta.env.DEV) {
-  const styleguideOptions = await import('@!production/setup/styleguide'); // eslint-disable-line import/no-webpack-loader-syntax
+  const styleguideOptions = await import('./setup/styleguide'); // eslint-disable-line import/no-webpack-loader-syntax
 
   vueOptions = {
     ...vueOptions,

00fdae4499a0a4c6210106a70fa1c5451a966f42
Philip Nachbauer <hello@haba.at>
2025-03-18 14:14:14 +0100

    removed

--- src/styleguide/mock-data/initial-data.ts
+++
@@ -1,6 +0,0 @@
-import { Store } from '@/setup/globals';
-import breadcrumbs from '@/styleguide/mock-data/initial-data/breadcrumbs';
-
-window.initialData = window.initialData || {};
-
-window.initialData[Store.Breadcrumb] = breadcrumbs;

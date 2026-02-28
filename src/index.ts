dc0d7541528394cbe8091700ecda98e0f7eca021
Philip Nachbauer <hello@haba.at>
2026-02-28 22:01:33 +0100

    Giving this project a good old boosting

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export { useVasSettingsStore } from './stores/settings';
+export type { VasSettingsStore } from './stores/settings';
+
+export * from './types/settings';
+
+export { default as cVasConfig } from './components/c-vas-config.vue';
+export { default as cVasFlyout } from './components/c-vas-flyout.vue';
+export { default as cVasSidebar } from './components/c-vas-sidebar.vue';
+export { default as cVasHtmlValidation } from './components/c-vas-html-validation.vue';
+
+export { default as eVasSelect } from './elements/e-vas-select.vue';
+export { default as eVasToggle } from './elements/e-vas-toggle.vue';
+export { default as eVasToggleButton } from './elements/e-vas-toggle-button.vue';

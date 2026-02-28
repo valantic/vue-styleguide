4a0e44efe20715350615a4f12dfa9bfd05bf47cc
Philip Nachbauer <hello@naba.at>
2026-02-28 22:28:22 +0100

    Giving this project a good old boosting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-export { useVasSettingsStore } from './stores/settings';
-export type { VasSettingsStore } from './stores/settings';
-
+// Types
 export * from './types/settings';
-
-export { default as cVasConfig } from './components/c-vas-config.vue';
-export { default as cVasFlyout } from './components/c-vas-flyout.vue';
-export { default as cVasSidebar } from './components/c-vas-sidebar.vue';
-export { default as cVasHtmlValidation } from './components/c-vas-html-validation.vue';
-
+export type { VasSettingsStore } from './stores/settings';
+export type { Options } from './elements/e-vas-select.vue';
+// Store
+export { useVasSettingsStore } from './stores/settings';
+// Elements
+export { default as eVasButton } from './elements/e-vas-button.vue';
 export { default as eVasSelect } from './elements/e-vas-select.vue';
 export { default as eVasToggle } from './elements/e-vas-toggle.vue';
 export { default as eVasToggleButton } from './elements/e-vas-toggle-button.vue';
+// Components
+export { default as cVasSidebar } from './components/c-vas-sidebar.vue';
+export { default as cVasDemoCard } from './components/c-vas-demo-card.vue';
+export { default as cVasDemoTable } from './components/c-vas-demo-table.vue';
+// Layouts
+export { default as lVasLayout } from './layouts/l-vas-layout.vue';

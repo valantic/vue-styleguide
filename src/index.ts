5b41790e0ef764f310b5047c273ad83b026466be
Philip Nachbauer <hello@naba.at>
2026-03-02 16:07:59 +0100

    Giving this project a good old boosting

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export type { VasSettingsStore } from './stores/settings';
 export { useVasSettingsStore } from './stores/settings';
 export { default as cVasSidebar } from './components/c-vas-sidebar.vue';
-export { default as lVasLayout } from './layouts/l-vas-layout.vue';

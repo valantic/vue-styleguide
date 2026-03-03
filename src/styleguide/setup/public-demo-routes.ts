f5429c00bbca7ce8df416f876e3d8ee60cd07712
Philip Nachbauer <hello@haba.at>
2026-03-03 15:46:05 +0100

    Giving this project a good old boosting

---
+++ src/styleguide/setup/public-demo-routes.ts
@@ -0,0 +1,11 @@
+export const styleguideTestPages = {
+  readme: {
+    path: 'sg-test-page-readme',
+    name: 'sg-test-page-readme',
+    component: () => import('../demo-pages/r-readme.vue'),
+    meta: {
+      title: 'Readme',
+      file: '/README.md',
+    },
+  },
+};

49040f8be71d4af703ac8944c56e320432a43606
Philip Nachbauer <hello@naba.at>
2026-05-22 11:33:34 +0200

    fixed import links

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-// DEPRECATED
-export { default as cVasDemoCard } from '../features/c-vas-demo-card.vue';
-export { default as cVasDemoTable } from '../features/c-vas-demo-table.vue';
-export { default as cVasRenderMarkdownFile } from '../features/c-vas-render-markdown-file.vue';
--- src/types/icon.d.ts
+++ src/types/icon.d.ts
@@ -1,23 +1,24 @@
 // Don't edit this file. Use the NPM script 'build:icons' instead.
-export type Icon = 'i-chevron--down' |
-  'i-close' |
-  'i-cog-wheel' |
-  'i-computer' |
-  'i-control-alt' |
-  'i-control' |
-  'i-folder' |
-  'i-key-cmd--filled' |
-  'i-key-cmd' |
-  'i-key-ctrl' |
-  'i-key-esc' |
-  'i-key-shift' |
-  'i-moon' |
-  'i-page-setting' |
-  'i-sun' |
-  'i-tag' |
-  'i-text' |
-  'i-valantic' |
-  'i-viewport--desktop' |
-  'i-viewport--mobile' |
-  'i-viewport--tablet' |
-  'i-vuejs';
+export type Icon =
+  | 'i-chevron--down'
+  | 'i-close'
+  | 'i-cog-wheel'
+  | 'i-computer'
+  | 'i-control-alt'
+  | 'i-control'
+  | 'i-folder'
+  | 'i-key-cmd--filled'
+  | 'i-key-cmd'
+  | 'i-key-ctrl'
+  | 'i-key-esc'
+  | 'i-key-shift'
+  | 'i-moon'
+  | 'i-page-setting'
+  | 'i-sun'
+  | 'i-tag'
+  | 'i-text'
+  | 'i-valantic'
+  | 'i-viewport--desktop'
+  | 'i-viewport--mobile'
+  | 'i-viewport--tablet'
+  | 'i-vuejs';
